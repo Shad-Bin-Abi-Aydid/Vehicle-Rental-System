725190c0930bd390aa3a02bc2f8e8ef08a3f3159
shad-bin-abi-aydid <shadaydid@gmail.com>
2026-01-31 15:29:44 +0000

    Implemented the Bearer token system

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -4,35 +4,52 @@ import config from "../config";
 import { pool } from "../config/db";
 
 const auth = (...roles: ("admin" | "customer")[]) => {
-  return async (req: Request, res: Response, next:NextFunction) => {
-    const token = req.headers.authorization;
-    if (!token) {
-      throw new Error("You are not authorized!");
-    }
-    // verify the token
-    const decoded = jwt.verify(
-      token,
-      config.jwt_secret as string,
-    ) as JwtPayload;
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const authHeader = req.headers.authorization;
 
-    const user = await pool.query(`SELECT * FROM users WHERE email=$1`, [
-      decoded.email,
-    ]);
+      if (!authHeader || !authHeader.startsWith("Bearer ")) {
+        throw new Error("You are not authorized!");
+      }
 
-    if(user.rows.length === 0){
+      const parts = authHeader.split(" ");
+      const token = parts[1];
+
+      if (!token) {
+        return res.status(401).json({
+          success: false,
+          message: "Token missing from Bearer header",
+        });
+      }
+
+      // verify the token
+      const decoded = jwt.verify(
+        token,
+        config.jwt_secret as string,
+      ) as JwtPayload;
+
+      const user = await pool.query(`SELECT * FROM users WHERE email=$1`, [
+        decoded.email,
+      ]);
+
+      if (user.rows.length === 0) {
         throw new Error("User not found");
-    }
+      }
 
-    req.user = decoded;
+      req.user = decoded;
 
-
-    // if user not the approprite role
-    if(roles.length && !roles.includes(decoded.role)){
+      // if user not the approprite role
+      if (roles.length && !roles.includes(decoded.role)) {
         throw new Error("You are not authorized");
+      }
+
+      next();
+    } catch (err: any) {
+      res.status(401).json({
+        success: false,
+        message: err.message || "Authentication Failed",
+      });
     }
-
-    next();
-
   };
 };
 
